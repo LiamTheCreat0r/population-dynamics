5b4dbac35e8c1b70cb37cdf3d234ae8a759fd272
Liam <106878018+LiamTheCreat0r@users.noreply.github.com>
2026-09-16 16:44:46 +0200

    feat: reorganisation in classes
    Co-authored-by: mathiscoulloch <contactmcoulloch-collab@users.noreply.github.com>

--- main.py
+++ main.py
@@ -3,54 +3,21 @@ import itertools
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-import numpy as np
+from modeles import Malthus
 
-from modeles import *
-
-croissance = 0.1
-
-POPULATION_INIT = 1000
-
-def data_gen():
-    for cnt in itertools.count():
-        t = cnt / 10
-        yield t, malthus(ydata[-1], croissance)  # last population, from ydata
-
-def init():
-    ax.set_ylim(POPULATION_INIT, POPULATION_INIT * 2)
-    ax.set_xlim(0, 1)
-    del xdata[:]
-    xdata.append(0)              # x = time, starts at 0
-    del ydata[:]
-    ydata.append(POPULATION_INIT)  # y = population, starts at POPULATION_INIT
-    line.set_data(xdata, ydata)
-    return line,
+growth = 1.05
 
 fig, ax = plt.subplots()
 line, = ax.plot([], [], lw=2)
 ax.grid()
-xdata, ydata = [], []
 
-def run(data):
-    # update the data
-    t, y = data
-    xdata.append(t)
-    ydata.append(y)
-    xmin, xmax = ax.get_xlim()
-    ymin, ymax = ax.get_ylim()
+modele = Malthus(ax, line)
 
-    if t >= xmax:
-        ax.set_xlim(xmin, xdata[-1])
-        ax.figure.canvas.draw()
-    if ydata[-1] >= ymax:
-        ax.set_ylim(POPULATION_INIT, ydata[-1])
-        ax.figure.canvas.draw()
+def data_gen():
+    for cnt in itertools.count():
+        t = cnt / 10
+        yield t, modele.generation(modele.ydata[-1], growth)
 
-    line.set_data(xdata, ydata)
-
-    return line,
-
-# Only save last 100 frames, but run forever
-ani = animation.FuncAnimation(fig, run, data_gen, interval=100, init_func=init,
-                              save_count=100)
+ani = animation.FuncAnimation(fig, modele.run, data_gen, interval=100,
+                              init_func=modele.init, save_count=100)
 plt.show()
--- modeles.py
+++ modeles.py
@@ -1,11 +1,59 @@
-def malthus(pop, r):
-    return pop + pop*r
+from abc import ABC, abstractmethod
 
-def verhulst():
-    ...
+from abc import ABC, abstractmethod
+
+
+class Modele(ABC):
+
+    POPULATION_INIT = 10
+
+    def __init__(self, ax, line):
+        self.ax = ax
+        self.line = line
+        self.xdata = []
+        self.ydata = []
+
+    @abstractmethod
+    def generation(self, *args):
+        """Calcule l'etat suivant a partir de l'etat courant."""
+
+    def init(self):
+        self.ax.set_ylim(self.POPULATION_INIT, self.POPULATION_INIT * 2)
+        self.ax.set_xlim(0, 1)
+        self.xdata.clear()
+        self.xdata.append(0)
+        self.ydata.clear()
+        self.ydata.append(self.POPULATION_INIT)
+        self.line.set_data(self.xdata, self.ydata)
+        return self.line
+
+    def run(self, data):
+        t, y = data
+        self.xdata.append(t)
+        self.ydata.append(y)
+
+        xmin, xmax = self.ax.get_xlim()
+        if t >= xmax:
+            self.ax.set_xlim(xmin, self.xdata[-1])
+            self.ax.figure.canvas.draw()
+
+        ymin, ymax = self.ax.get_ylim()
+        if y >= ymax:
+            self.ax.set_ylim(ymin, self.ydata[-1])
+            self.ax.figure.canvas.draw()
+
+        self.line.set_data(self.xdata, self.ydata)
+        return self.line
+
+
+class Malthus(Modele):
+
+    def generation(self, population, growth):
+        return population * growth
+
+def verhulst(population, growth, environnementCapacity):
+    return population + population*growth*(1-population/environnementCapacity)
     
-def rungeKutta():
-    ...
-
-def volterra():
-    ...
+def volterra(populationPrey, populationPredator,growthPrey,growthPredator,chanceInteractionPrey,chanceInteractionPredtor):
+    preyEvolution = growthPrey*populationPrey - chanceInteractionPrey*populationPrey*populationPredator
+    predatorEvolution = -growthPredator*populationPredator + chanceInteractionPredtor*populationPrey*populationPredator
